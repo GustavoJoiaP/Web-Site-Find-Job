5fb35d8c3e2a79a9f5c71e0032119d4878efb4a0
GustavoJoiaP <joia.gustavo@gmail.com>
2021-02-16 18:31:22 -0300

    meu segundo commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const express    = require('express');
+const exphbs     = require('express-handlebars');
+const app        = express();
+const path       = require('path');
+const db         = require('./db/connection');
+const bodyParser = require('body-parser');
+const Job        = require('./models/Job'); 
+const Sequelize  = require('sequelize');
+const { query } = require('express');
+const Op         = Sequelize.Op;
+
+const PORT = 7000;
+
+app.listen(PORT, function(){
+    console.log("O express esta rodando  na portal: " + PORT);
+});
+
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// handler bars
+app.set("views", path.join(__dirname,'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// db connection
+db
+    .authenticate()
+    .then(() =>{
+        console.log("Conectou ao banco com sucesso!");
+    })
+    .catch(err =>{ 
+        console.log("Ocorreu um erro ao tentar conectar ao Banco de Dados", err);
+    })
+
+
+
+// routes
+app.get('/', (req, res) => {
+
+    let search = req.query.job;
+    let query  = '%' + search + '%';
+
+    if(!search){
+      Job.findAll({order: [
+        ['createdAt', 'DESC']
+      ]})
+      .then(jobs => {
+    
+        res.render('index', {
+          jobs
+        });
+    
+      })
+      .catch(err => console.log(err));
+    }else{
+      Job.findAll({
+        where: {title: {[Op.like]: query}},
+        order: [
+        ['createdAt', 'DESC']
+      ]})
+      .then(jobs => {
+    
+        res.render('index', {
+          jobs, search
+        });
+    
+      })
+      .catch(err => console.log(err))
+    }
+    
+});
+
+// jobs routes
+
+app.use('/jobs', require('./routes/jobs'));
---
+++ routes/jobs.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router  = express.Router();
+const Job     = require('../models/Job');
+
+// test rout
+router.get("/test",(req,res) => {
+    res.send("deu certo!")
+});
+
+// detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+  }).then(job => {
+
+    res.render('view', {
+      job
+    })
+
+  }).catch(err => console.log(err)));
+
+
+// form da rota de envio
+router.get('/add',(req, res) => {
+    res.render('add');
+});
+
+// Adicionar um job via post. Ira criar uma rota para adicionar!
+// add job via post
+router.post('/add', (req, res) => {
+
+  let {title, salary, company, description, email, new_job} = req.body;
+
+  // insert
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+    new_job
+  })
+  .then(() => res.redirect('/'))
+  .catch(err => console.log(err));
+
+});
+
+module.exports = router
